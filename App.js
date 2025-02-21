b71f79c2eed6e9f98aeca221a729ede271359671
BugHunter-X <askzenai@gmail.com>
2025-02-21 02:38:04 +0100

    Add App.js file

---
+++ App.js
@@ -0,0 +1,14 @@
+import { registerRootComponent } from 'expo';
+
+// ... other imports
+
+registerRootComponent(App);
+
+function App(){
+    // ... other code
+    return (
+        <View>
+          <Text>Hello world!</Text>
+        </View>
+    );
+}
